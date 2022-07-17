ee290d5d39638d6401f32ec59225359340142e04
Raaj Kancan <iamraajkanchan@gmail.com>
2022-07-17 22:23:10 +0530

    Concurrency.kt- Added

---
+++ src/main/kotlin/Rough/Concurrency.kt
@@ -0,0 +1,24 @@
+package Rough
+
+fun main() {
+    val thread = Thread {
+        println("${Thread.currentThread().name} is running")
+    }
+    thread.start()
+
+    val thread1 = Thread {
+        println("${Thread.currentThread().name} is running")
+    }
+    thread1.start()
+
+    val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+    repeat(3) {
+        Thread {
+            println("${Thread.currentThread().name} has started")
+            for(item in states) {
+                println("${Thread.currentThread()} - $item")
+                Thread.sleep(500)
+            }
+        }.start()
+    }
+}
